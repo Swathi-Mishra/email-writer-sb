bd0170fa2cb5f645a88ecc9fc9016fa18aa91ac7
Swathi Mishra <swathimishra150@gmail.com>
2025-03-08 12:40:15 +0530

    Configured port log binding

--- src/main/java/com/email/writer/EmailWriterSbApplication.java
+++ src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -1,26 +1,15 @@
 package com.email.writer;
 
-//import java.util.logging.Logger;
-import org.slf4j.Logger;
-
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class EmailWriterSbApplication {
-	private static final Logger logger = LoggerFactory.getLogger(EmailWriterSbApplication.class);
 
 	public static void main(String[] args) {
+		String port = System.getenv("PORT") != null ? System.getenv("PORT") : "8080";
+        System.getProperties().put("server.port", port); // Programmatically setting port
 		SpringApplication.run(EmailWriterSbApplication.class, args);
-
-		SpringApplication app = new SpringApplication(EmailWriterSbApplication.class);
-        ConfigurableApplicationContext context = app.run(args);
-
-        // Log the active port
-        int port = context.getEnvironment().getProperty("local.server.port", Integer.class, 8080);
-        logger.info("Application started successfully on port: {}", port);
 	}
 
 }
